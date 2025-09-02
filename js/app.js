e4364a75e00af11ad405917f56db81306cd90c0c
Dan Susa <dansousa@tutamail.com>
2025-09-02 08:52:58 -0300

    Under Construction Web Site

---
+++ js/app.js
@@ -0,0 +1,53 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+
+function PageTransitions(){
+    // Button click active class
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function() {
+            const currentBtn = document.querySelector('.active-btn');
+            if (currentBtn) {
+                currentBtn.classList.remove('active-btn');
+            }
+            this.classList.add('active-btn');
+            //let currentBtn = document.querySelectorAll('.active-btn');
+            //currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            //this.className += ' active-btn';
+        })
+    }
+
+    //Sections Active
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            // Remove selected from the other btns
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            // Hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+    /*
+    // Deselect buttons on outside click
+    document.addEventListener('click', function(e) {
+        if (!e.target.closest('.control')) {
+            const activeBtn = document.querySelector('.active-btn');
+            if (activeBtn) {
+                activeBtn.classList.remove('active-btn');
+            }
+            }
+    });
+    */
+}
+PageTransitions();
